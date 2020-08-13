ad19f48da9f2ae91e080ced5313e7be83b13e057
YeriNbow <jwjln3126@naver.com>
2020-08-13 21:11:16 +0900

    Create db.py
    Save lyrics to MySQL Database

---
+++ db.py
@@ -0,0 +1,40 @@
+import pymysql
+
+
+class ScrapeDB:
+    """
+    Save data at MySQL Database
+    """
+    def __init__(self):
+        self.con = pymysql.connect(host='127.0.0.1', user='root', password='******************',
+                                   db='testdb', cursorclass=pymysql.cursors.DictCursor)
+        self.curs = self.con.cursor()
+
+    def create_table(self):
+        sql = '''
+                CREATE table IF NOT EXISTS scrape_tb(
+                    Artist varchar(50),
+                    Url varchar(200),
+                    Title varchar(200),
+                    Lyrics varchar(5000)
+                );
+                '''
+        self.curs.execute(sql)
+        self.con.commit()
+
+    def insert_data(self, artist, url, title, lyrics):
+        sql = '''
+                INSERT INTO scrape_tb
+                values(%s, %s, %s, %s)
+                '''
+        self.curs.execute(sql, (artist, url, title, lyrics))
+        self.con.commit()
+
+    def get_data(self):
+        sql = '''
+            SELECT *
+            FROM scrape_tb
+            '''
+        self.curs.execute(sql)
+
+        return self.curs.fetchall()
